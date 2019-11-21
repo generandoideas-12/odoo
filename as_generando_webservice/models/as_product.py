88e7526ea693614bbf57a81d6e8e2f9c1390d77c
Carlos Paz <phoenixsampras@gmail.com>
2019-11-21 17:08:10 -0400

    modulo consumo de webservice reportes

--- as_generando_webservice/models/as_product.py
+++ as_generando_webservice/models/as_product.py
@@ -11,7 +11,11 @@ class as_ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     as_codigo_proveedor = fields.Char(string="Codigo Proveedor")
+    as_name_proveedor = fields.Char(string="Nombre Proveedor")
     as_costo_proveedor = fields.Float(string="Costo Proveedor", default=0)
+    as_costo_anterior = fields.Float(string="Costo Anterior", default=0)
     as_existencias = fields.Float(string="Existencia Proveedor", default=0)
     as_factor = fields.Float('Factor Precio', default=1)
+    as_descontinuado = fields.Boolean('Descontinuado', default=True)
+
     
