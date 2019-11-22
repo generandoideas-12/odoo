20bab5ec0a4e8eeecd41a1e8137e4980abe45f62
Carlos Paz <phoenixsampras@gmail.com>
2019-11-22 15:56:29 -0400

    customizaciones ventas

---
+++ as_generando_sale/models/as_sale_order.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.misc import formatLang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+
+from werkzeug.urls import url_encode
+
+import logging
+logger = logging.getLogger(__name__)
+
+class as_SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def line_product_edit(self):
+        posiciones_insigneas = []
+        insignea=0
+        bandera = False
+        for line in self.order_line:
+            if line.product_id.product_tmpl_id.x_studio_producto_insignia == True:
+                line_venta={}
+                if insignea == 0 and bandera== False:
+                    bandera=True
+                    insignea=0
+                elif bandera==True:
+                    insignea+=1
+                line_venta= {
+                        'price_total': line.price_total,
+                        'display_type': line.display_type,
+                        'display_type': line.display_type,
+                        'display_type': line.display_type,
+                        'name': line.name,
+                        'product_uom_qty': line.product_uom_qty,
+                        'product_uom': line.product_uom.name,
+                        'price_unit': line.price_unit,
+                        'discount': line.discount,
+                        'tax_id': line.tax_id,
+                        'price_subtotal': line.price_subtotal,
+                        'price_total': line.price_total,
+                        'display_type ': line.display_type,
+                        'name': line.name,
+                        'display_type ': line.display_type ,
+                        'name': line.name,
+                        'material': line.product_id.product_tmpl_id.x_studio_material,
+                        'medidas': line.product_id.product_tmpl_id.x_studio_medidas,
+                        'aplicacion': '',
+                    }
+                posiciones_insigneas.append(line_venta)
+            else:
+                if line.product_id.product_tmpl_id.x_studio_sumar_descripcin_1 == True:
+                    posiciones_insigneas[insignea]['name']= posiciones_insigneas[insignea]['name']+'/'+line.name
+                    posiciones_insigneas[insignea]['price_subtotal']+=line.price_subtotal
+                elif line.product_id.product_tmpl_id.x_studio_sumar_descripcin_1 == False and line.product_id.product_tmpl_id.x_studio_aplicacin_1 == False:
+                    posiciones_insigneas[insignea]['price_subtotal']+=+line.price_subtotal
+                if line.product_id.product_tmpl_id.x_studio_aplicacin_1 == True:
+                    posiciones_insigneas[insignea]['aplicaciobol']= True
+                    posiciones_insigneas[insignea]['aplicacion']= line.name
+                    posiciones_insigneas[insignea]['price_subtotal']+=line.price_subtotal
+                
+        return posiciones_insigneas
+                    
+        
