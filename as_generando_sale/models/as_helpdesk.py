2f3500f40ae7d18ed96c911f96a7f6a88388c7b3
Carlos Paz <phoenixsampras@gmail.com>
2020-02-18 10:03:16 -0400

    colaborativo v1.2

--- as_generando_sale/models/as_helpdesk.py
+++ as_generando_sale/models/as_helpdesk.py
@@ -9,11 +9,11 @@ from datetime import datetime, timedelta
 class as_HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    partner_name = fields.Char(string='Customer Name',default=_get_default_partner)
+    # partner_name = fields.Char(string='Customer Name',default=_get_default_partner)
 
-    @api.model
-    def _get_default_partner(self):
-        ctx = self._context
-        if ctx.get('active_model') == 'sale.order':
-            return self.env['sale.order'].browse(ctx.get('active_ids')[0]).partner_id.id
+    # # @api.model
+    # # def _get_default_partner(self):
+    # #     ctx = self._context
+    # #     if ctx.get('active_model') == 'sale.order':
+    # #         return self.env['sale.order'].browse(ctx.get('active_ids')[0]).partner_id.id
         
