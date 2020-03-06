4554175e77577d61eb485e1c88b3c32c64aafa3f
Carlos Paz <phoenixsampras@gmail.com>
2020-03-06 10:21:58 -0400

    webservice pagina 2.0

--- as_generando_webservice/models/as_importar_productos.py
+++ as_generando_webservice/models/as_importar_productos.py
@@ -11,6 +11,9 @@ from odoo.osv import expression
 from odoo.tools import float_is_zero, float_compare
 from tabulate import tabulate
 import pandas as pd
+import csv
+import base64
+from io import StringIO
 
 from werkzeug.urls import url_encode
 
@@ -32,6 +35,7 @@ class as_importar_productos(models.Model):
     
     name = fields.Char('Comentario', default='Importacion manual')
     as_url = fields.Char('Endpoint', default='https://www.generandoideas.com/conexionodoo/esi_api/productos')
+    as_url_full = fields.Char('Full Endpoint', default='https://www.generandoideas.com/conexionodoo/esi_api/productos')
     as_login = fields.Char('Usuario', default='admin')
     as_password = fields.Char('Password', default='eIsTi_1987API')
     as_iteracion = fields.Integer('Frecuencia de almacenamiento', default=100)
@@ -137,9 +141,14 @@ class as_importar_productos(models.Model):
             response = requests.get(operacion.as_url,auth=requests.auth.HTTPBasicAuth(operacion.as_login,operacion.as_password))
             jsondata = json.loads(response.text)
             
+            # Full Json Data
+            response2 = requests.get(operacion.as_url_full,auth=requests.auth.HTTPBasicAuth(operacion.as_login,operacion.as_password))
+            jsondata2 = json.loads(response2.text)
+            
             if operacion.as_catalogo in jsondata:
                 
                 jsondata = jsondata[operacion.as_catalogo]
+                jsondata2 = jsondata2[operacion.as_catalogo]
                 
                 jsondata = self.take(operacion.as_limit, jsondata)
                 
@@ -151,12 +160,6 @@ class as_importar_productos(models.Model):
                 counter = 0
                 total_elapsed = 0
                 nro_orders = len(jsondata)
-
-
-
-
-
-
 
                 if value.get('create_tf'):
                     self.env.cr.execute("""select as_codigo_proveedor from product_template """)
@@ -184,11 +187,10 @@ class as_importar_productos(models.Model):
                             count += 1
                                 # raise Warning(_('"%s" Product not found.') % values.get('default_code'))
 
-                        # Escribir cada cierta iteracion
-                        # if count == operacion.as_iteracion:
-                        #     self.env.cr.commit()
-                        #     break
-                        #     count = 0
+                            # Escribir cada cierta iteracion
+                            if count == operacion.as_iteracion:
+                                self.env.cr.commit()
+                                count = 0
 
                         # Tiempo transcurrido
                         elapsed = round((time.time() - start),2)
@@ -199,7 +201,7 @@ class as_importar_productos(models.Model):
                         # if counter == operacion.as_iteracion:
                         #     break
 
-                        _logger.info("\n\nNro: %s Operacion: %s Porcentaje: %s Registro confirmado: %s Segundos: %s TOTAL TIEMPO: %s", counter, tipo_operacion, percentage, value.get('CODPROD'), elapsed, round((total_elapsed/60),2))
+                        _logger.info("\n\nNro: %s Operacion: %s Porcentaje: %s Registro confirmado: %s Segundos: %s TOTAL TIEMPO: %s URL: %s", counter, tipo_operacion, percentage, value.get('CODPROD'), elapsed, round((total_elapsed/60),2), operacion.as_url)
 
                 elif value.get('update_tf'):
 
@@ -211,7 +213,7 @@ class as_importar_productos(models.Model):
                     old_json_data = [z for z in jsondata if z['CODPROD'] in productos]
 
                     self.env.cr.execute("""
-                                select pp.id, pt.name, pt.as_codigo_proveedor, pt.as_costo_proveedor, pt.as_costo_anterior, pt.as_name_proveedor, pt.as_existencias, pt.list_price, pt.as_factor, pt.as_descontinuado from product_product pp, product_template pt where tf_check_update = 'no_update' and pp.product_tmpl_id = pt.id limit %s
+                                select pp.id, pt.name, pt.as_codigo_proveedor, pt.as_costo_proveedor, pt.as_costo_anterior, pt.as_name_proveedor, pt.as_existencias, pt.list_price, pt.as_factor, pt.as_descontinuado from product_product pp, product_template pt where pp.product_tmpl_id = pt.id -- limit %s
                             """ % (self.as_iteracion+150))
                     result = self.env.cr.dictfetchall()
                     
@@ -248,22 +250,25 @@ class as_importar_productos(models.Model):
                             # _logger.info('Update number %s'%str(count))
                             # _logger.info('Product Info')
                             # _logger.info(pro_id)
+                            
+                            # Escribir cada cierta iteracion
+                            if count == operacion.as_iteracion:
+                                self.env.cr.commit()
+                                count = 0                            
                             count+=1
 
-                        # Tiempo transcurrido
-                        elapsed = round((time.time() - start),2)
-                        total_elapsed = elapsed + total_elapsed
-                        counter = counter + 1
-                        percentage = round(float(counter) / nro_orders * 100,2)                            
+                            # Tiempo transcurrido
+                            elapsed = round((time.time() - start),2)
+                            total_elapsed = elapsed + total_elapsed
+                            counter = counter + 1
+                            percentage = round(float(counter) / nro_orders * 100,2)
 
-                        _logger.info("\n\nNro: %s Operacion: %s Porcentaje: %s Registro confirmado: %s Segundos: %s TOTAL TIEMPO: %s", counter, tipo_operacion, percentage, value.get('CODPROD'), elapsed, round((total_elapsed/60),2))
+                            _logger.info("\n\nNro: %s Operacion: %s Porcentaje: %s Registro confirmado: %s Segundos: %s TOTAL TIEMPO: %s URL: %s", counter, tipo_operacion, percentage, value.get('CODPROD'), elapsed, round((total_elapsed/60),2), operacion.as_url)
 
                     if not result:
                         _logger.info('All Product set to state no update!')
                         self.env.cr.execute("""update product_template set tf_check_update = 'no_update';""")
 
-                    # start = time.time()
-                    # time.clock()
                     # self.env.cr.commit()
 
                     # elapsed = round((time.time() - start), 2)
@@ -289,26 +294,52 @@ class as_importar_productos(models.Model):
                 body += "<b>Total Productos Locales: </b>%s <br>" %(str(self.as_total_productos_actuales()))
                 body += "<b>Tiempo (Segundos): </b>%s <br>" %(round(total_elapsed,2))
                 
+                ## JSON
+                attach_id = self.as_generar_csv(jsondata,"datos_remotos" + str(operacion.id) + ".csv")
+                attach_url = "<a href='/web/content/" + str(attach_id.id) + "?download=true' download='Datos Remotos JSON " + str(attach_id.id) + "'>Datos Remotos JSON " + str(attach_id.id) + "</a>"
                 # table = tabulate(jsondata,headers,tablefmt='html')
                 # table = table.replace('<table>', '<table class="oe_list_content" border="1" style="border-collapse:collapse;">')
-                # body += "<b>REST Importados: </b></br>%s <br>" %(table)
+                body += "<b>Datos REST Importados: </b></br>%s <br>" %(attach_url)
                 
+                ## DIFERENCIAS
                 # table_diferencias = tabulate(self.as_diferencias(jsondata),headers,tablefmt='html')
                 # table_diferencias = table_diferencias.replace('<table>', '<table class="oe_list_content" border="1" style="border-collapse:collapse;">')
                 # body += "<b>DIFERENCIA entre DB LOCAL y Remoto: </b></br>%s <br>" %(table_diferencias)
+                ## Crear CSV
+                # table_diferencias = self.as_diferencias(jsondata2)
+                # attach_id2 = self.as_generar_csv2(table_diferencias,"datos_diferencias" + str(operacion.id) + ".csv",True)
+                # attach_url2 = "<a href='/web/content/" + str(attach_id2.id) + "?download=true' download='Datos Remotos JSON " + str(attach_id2.id) + "'>Datos Remotos " + str(attach_id2.id) + "</a>"
+                # body += "<b>DIFERENCIA entre DB LOCAL y Remoto sin paginacion: </b></br>%s <br>" %(attach_url2)
                 
+                ## JSON UNICOS
                 # table_unicos = tabulate(self.as_remover_duplicados(jsondata),headers,tablefmt='html')
                 # table_unicos = table_unicos.replace('<table>', '<table class="oe_list_content" border="1" style="border-collapse:collapse;">')
                 # body += "<b>REST Importados Unicos: </b></br>%s <br>" %(table_unicos)
+                ## Crear CSV
+                # table_unicos = self.as_remover_duplicados(jsondata)
+                # attach_id3 = self.as_generar_csv(table_unicos,"table_unicos" + str(operacion.id) + ".csv")
+                # attach_url3 = "<a href='/web/content/" + str(attach_id.id) + "?download=true' download='Datos Remotos JSON " + str(attach_id.id) + "'>Datos Remotos " + str(attach_id.id) + "</a>"
+                # body += "<b>REST Importados Unicos: </b></br>%s <br>" %(attach_url3)
                 
+                ## JSON DUPLICADOS
                 # table_duplicados = tabulate(self.as_duplicados(jsondata),headers,tablefmt='html')
                 # table_duplicados = table_duplicados.replace('<table>', '<table class="oe_list_content" border="1" style="border-collapse:collapse;">')
                 # body += "<b>REST Importados Duplicados: </b></br>%s <br>" %(table_duplicados)
-                                
+                ## Crear CSV
+                table_duplicados = self.as_duplicados(jsondata)
+                attach_id4 = self.as_generar_csv2(table_duplicados,"table_duplicados" + str(operacion.id) + ".csv",True)
+                attach_url4 = "<a href='/web/content/" + str(attach_id4.id) + "?download=true' download='REST Importados Duplicados " + str(attach_id4.id) + "'>REST Importados Duplicados " + str(attach_id4.id) + "</a>"
+                body += "<b>REST Importados Duplicados: </b></br>%s <br>" %(attach_url4)
+                
+                ## LOCAL DUPLICADOS
                 # table_duplicados_local = tabulate(self.as_repetidos(),headers,tablefmt='html')
                 # table_duplicados_local = table_duplicados_local.replace('<table>', '<table class="oe_list_content" border="1" style="border-collapse:collapse;">')
                 # body += "<b>DB Duplicados: </b></br>%s <br>" %(table_duplicados_local)
-
+                ## Crear CSV
+                table_duplicados_local = self.as_repetidos()
+                attach_id5 = self.as_generar_csv2(table_duplicados_local,"table_duplicados_local" + str(operacion.id) + ".csv",False)
+                attach_url5 = "<a href='/web/content/" + str(attach_id5.id) + "?download=true' download='DB Duplicados " + str(attach_id5.id) + "'>DB Duplicados " + str(attach_id5.id) + "</a>"
+                body += "<b>DB Duplicados: </b></br>%s <br>" %(attach_url5)
 
                 # body += 
                 operacion.message_post(body = body)
@@ -362,3 +393,80 @@ class as_importar_productos(models.Model):
         
         df_1notin2 = df1[~(df1['CODPROD'].isin(df2['CODPROD']) & df1['NOMPROD'].isin(df2['NOMPROD']))].reset_index(drop=True)
         return df_1notin2
+
+    def as_eliminar_adjuntos(self,operacion):
+        self.env['ir.attachment'].search([('res_model','=','as.importar.productos'),('res_id','=',operacion.id)]).unlink()
+    
+    def as_generar_csv(self,x,filename):
+        archivo = StringIO()
+        f = csv.writer(archivo)
+
+        # Write CSV Header, If you dont need that, remove this line
+        f.writerow([
+            'CODPROD',
+            'NOMPROD',
+            'COSUNIT',
+            'EXISTENCIAS',
+            'PROVEEDOR'
+        ])
+
+        for x in x:
+            f.writerow([
+                x["CODPROD"],
+                x["NOMPROD"],
+                x["COSUNIT"],
+                x["EXISTENCIAS"],
+                x["PROVEEDOR"],
+            ])
+            
+        # file_data = f.read()
+        out = base64.encodestring(bytes(archivo.getvalue(), encoding = 'utf-8'))
+        
+        # csv.writer(archivo)
+        
+        attach_id = self.env['ir.attachment'].create({
+            'name': filename,
+            'type': 'binary',
+            'datas': out,
+            'res_model': 'as.importar.productos',
+            'res_id': self.id,
+            'mimetype': 'text/csv'
+        })
+        
+        return attach_id
+    
+    def as_generar_csv2(self,x,filename,panda=True):
+
+        if panda:
+            x = x.values.tolist()
+
+        archivo = StringIO()
+        f = csv.writer(archivo)
+
+        # Write CSV Header, If you dont need that, remove this line
+        f.writerow([
+            'CODPROD',
+            'NOMPROD',
+        ])
+
+        for y in x:
+            f.writerow([
+                y[0],
+                y[1],
+            ])
+            
+        # file_data = f.read()
+        out = base64.encodestring(bytes(archivo.getvalue(), encoding = 'utf-8'))
+        
+        # csv.writer(archivo)
+        
+        attach_id = self.env['ir.attachment'].create({
+            'name': filename,
+            'type': 'binary',
+            'datas': out,
+            'res_model': 'as.importar.productos',
+            'res_id': self.id,
+            'mimetype': 'text/csv'
+        })
+        
+        return attach_id
